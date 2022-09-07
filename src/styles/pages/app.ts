fc9586b19010a990b30276878dc44b3de2fb4ea2
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-09-07 20:05:51 -0300

    arquivo de estilos para _app

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100vh',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+})
+
+export const Header = styled('header', {
+  width: '100%',
+  maxWidth: 1180,
+  padding: '2rem 0',
+  margin: '0 auto',
+})
