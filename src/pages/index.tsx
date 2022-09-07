4fa1f3b25576c1b3462db48443a57030e3b89750
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-09-07 10:13:47 -0300

    Instalando e configurando stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$primary',
+  padding: '10px 20px',
+  borderRadius: '5px',
+  border: 0,
+  color: 'white',
+  fontSize: '1.2rem',
+  cursor: 'pointer',
+  '&:hover': {
+    filter: 'brightness(0.9)'
+  },
+})
+
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div>
+      <h1>Hello World</h1>
+      <Button>Teste</Button>
+    </div>
   )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,19 @@
+import { createStitches } from '@stitches/react'
+
+export const { 
+  config,
+  createTheme,
+  globalCss,
+  theme,
+  keyframes,
+  css,
+  styled,
+  getCssText,
+} = createStitches({
+  theme: {
+    colors: {
+      primary: '#8257e6',
+    },
+  }
+})
+
