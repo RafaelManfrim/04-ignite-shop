d3d523d8d229f037f466a73c3c8a2114f60bfbde
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-09-08 14:37:05 -0300

    desabilitando o prefetch de todos os links da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => (
-        <Link key={product.id} href={`/product/${product.id}`}>
+        <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
           <Product className="keen-slider__slide" >
             <Image src={product.imageUrl} width={520} height={480} alt="" />
             <footer>
